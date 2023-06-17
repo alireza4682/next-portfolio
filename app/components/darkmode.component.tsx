2c14a33b7a64862532f9876bfd11a41af4d57161
alireza <comolestyrusistantaneos@gmail.com>
2023-06-17 19:00:34 +0330

    added switch

--- app/components/darkmode.component.tsx
+++ app/components/darkmode.component.tsx
@@ -1,10 +1,25 @@
 import { useTheme } from "next-themes";
+import { Switch } from "./ui/switch";
+import { useState } from "react";
+
 const DarkButton = () => {
   const { theme, setTheme } = useTheme();
+  const initialTheme = false;
+  const [booleanTheme, setBooleanTheme] = useState(initialTheme);
+  console.log(initialTheme);
+
   const darkModeHandler = () => {
-    theme === "dark" ? setTheme("light") : setTheme("dark");
+    if (theme === "dark") {
+      setTheme("light");
+      setBooleanTheme(false);
+    } else {
+      setTheme("dark");
+      setBooleanTheme(true);
+    }
   };
-  return <button onClick={() => darkModeHandler()}>dark</button>;
+  return (
+    <Switch checked={booleanTheme} onCheckedChange={() => darkModeHandler()} />
+  );
 };
 
 export default DarkButton;
