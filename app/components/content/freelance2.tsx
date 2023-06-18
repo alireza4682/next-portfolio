1840c3b93af90ed450f6488f67103f7c6b94e6a3
Alireza <comolestyrusistantaneos@gmail.com>
2023-06-18 23:29:52 +0330

    changed to p tag

--- app/components/content/freelance2.tsx
+++ app/components/content/freelance2.tsx
@@ -1,13 +1,15 @@
 const Freelance2 = () => {
   return (
-    <div className="prose prose-sm dark:prose-invert list-image-[url(checkmark.png)]">
+    <div className="prose prose-sm dark:prose-invert">
       <h4>Freelance</h4>
       <ul>
         <li>
-          worked with 2 startups in "Shiraz startup factory" to ensure their
-          libraries type safety and polished their frontend to be pixel perfect
-          and have industry level standards and minimize discrepancies before
-          presenting to investors.
+          <p className="ml-4">
+            worked with 2 startups in "Shiraz startup factory" to ensure their
+            libraries type safety and polished their frontend to be pixel
+            perfect and have industry level standards and minimize discrepancies
+            before presenting to investors.
+          </p>
         </li>
         <li>
           implemented continuous integration with "github actions" and end to
