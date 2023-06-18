66d5e3378f6bd00af33f9c40ee6c1f1ec97f7dfd
Alireza <comolestyrusistantaneos@gmail.com>
2023-06-18 19:16:11 +0330

    added 1 paragraph

--- app/content/freelance2.tsx
+++ app/content/freelance2.tsx
@@ -1,11 +1,20 @@
 const Freelance2 = () => {
   return (
-    <div className="prose prose-sm dark:prose-invert">
-      <h1>freelance</h1>
-      <p>
-        used React&#39;s best practices, code splitting and render optimization
-        to make our customer&#39;s websites load 30% faster and 15% smoother
-      </p>
+    <div className="prose prose-sm dark:prose-invert list-image-checkmark ">
+      <h4>Freelance</h4>
+      <ul>
+        <li>
+          worked with 2 startups in "Shiraz startup factory" to ensure their
+          libraries type safety and polished their frontend to be pixel perfect
+          and have industry level standards and minimize discrepancies before
+          presenting to investors.
+        </li>
+        <li>
+          implemented continuous integration with "github actions" and end to
+          end testing with "playwright" for a project I helped to start with
+          docker, supabase and react.
+        </li>
+      </ul>
       <p>
         with writing unit tests, using storybook.js and migrating from redux to
         redux toolkit, median time to design and develop components was improved
