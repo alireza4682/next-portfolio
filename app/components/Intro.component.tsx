aae772f23e2522cfc85acdff3599f934dc592120
Alireza <comolestyrusistantaneos@gmail.com>
2023-06-22 23:06:04 +0330

    useintersection

--- app/components/Intro.component.tsx
+++ app/components/Intro.component.tsx
@@ -2,24 +2,13 @@ import Image from "next/image";
 import me from "../../public/me.jpg";
 import Link from "next/link";
 import CvDownload from "./cvDownload.component";
+import IntroNav from "./introNav.component";
+
 
 const Intro = () => {
   return (
     <div className="flex flex-col h-auto md:w-1/3  items-center dark:secondary">
-      <div className=" flex w-64 h-64 rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 justify-center items-center m-4">
-        {/* <Image
-          src={me}
-          alt="me"
-          className="w-full h-full object-cover rounded-full p-1.5 contrast-125"
-          priority={true}
-          width={244}
-          height={244}
-        /> */}
-      </div>
-      <div className="">Alireza Arezoumandan</div>
-      <div className="text-sm">
-        Frontend engineer with 5+ years of experience
-      </div>
+      <IntroNav />
       <div className="m-4">
         5+ years of experience in JavaScript and 3+ years of experience in
         Reactjs, TypeScript, Redux and RESTful APIs. 2+ years of experience
--- app/components/introNav.component.tsx
+++ app/components/introNav.component.tsx
@@ -1,15 +1,20 @@
+'use client'
 import Image from "next/image";
-import useIsInViewport from "../hooks/useIsInVeiwport";
 import { NavigationMenu, NavigationMenuItem } from "./ui/navbar";
+import { useIntersectionObserver } from "../hooks/useIntersectionObserver.hook";
 
 import me from "../../public/me.jpg";
 import { useRef } from "react";
 
 function IntroNav() {
+  const ref = useRef<HTMLDivElement | null>(null)
+  const entry = useIntersectionObserver(ref, { threshold: 0})
+  const isVisible = !!entry?.isIntersecting
   return (
     <div>
-      <div className=" flex w-64 h-64 rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 justify-center items-center m-4">
-        {/* <Image
+      {isVisible ?
+        <div className=" flex w-64 h-64 rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 justify-center items-center m-4" ref={ref}>
+          {/* <Image
           src={me}
           alt="me"
           className="w-full h-full object-cover rounded-full p-1.5 contrast-125"
@@ -17,7 +22,10 @@ function IntroNav() {
           width={244}
           height={244}
         /> */}
-      </div>
+        </div>
+        :
+        <div>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</div>
+      }
       <div className="">Alireza Arezoumandan</div>
       <div className="text-sm">
         Frontend engineer with 5+ years of experience
