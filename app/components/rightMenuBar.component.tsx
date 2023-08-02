25ec9bb12572a0b3891c4b608bec67eb145f2d64
alireza <comolestyrusistantaneos@gmail.com>
2023-08-03 00:19:41 +0330

    simpler right menubar

--- app/components/rightMenuBar.component.tsx
+++ app/components/rightMenuBar.component.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React from "react";
-import { useReducer } from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 import WorkSection from "./workSection.component";
 import Skills from "./skills.component";
@@ -8,53 +7,37 @@ import { Separator } from "./ui/seperator";
 import ThemeSwitch from "./themeChanger";
 
 const choices = {
-  first: "first",
-  second: "second",
-  third: "third",
+  first: <WorkSection />,
+  second: <Skills />,
+  third: <div>wtf</div>,
 } as const;
 
-type ObjectType<T> = T[keyof T];
-
-type TActionChoices = ObjectType<typeof choices>;
-
-const rightMenuBarReducer = (
-  state: React.ReactElement,
-  action: TActionChoices
-) => {
-  switch (action) {
-    case "first":
-      return (state = <WorkSection />);
-    case "second":
-      return (state = <Skills />);
-    case "third":
-      return (state = <div>wtf</div>);
-    default:
-      throw new Error(`can't do that: ${action}`);
-  }
+const sectionState = (state: keyof typeof choices) => {
+  return <div>{choices[state]}</div>;
 };
 
 function RightMenuBar() {
-  const [state, dispatch] = useReducer(rightMenuBarReducer, <WorkSection />);
+  const [section, setSection] = useState<keyof typeof choices>("first");
   return (
     <div className="mt-20">
       <div className="flex flex-row justify-between items-center pr-2">
         <div className="flex flex-row justify-start gap-1 lg:gap-4 p-2 h-14">
-          <Button onClick={() => dispatch("first")} variant="ghost">
+          <Button onClick={() => setSection("first")} variant="ghost">
             workSection
           </Button>
           <Separator orientation="vertical" />
-          <Button onClick={() => dispatch("second")} variant="ghost">
+          <Button onClick={() => setSection("second")} variant="ghost">
             Skills & Tools
           </Button>
           <Separator orientation="vertical" />
-          <Button onClick={() => dispatch("third")} variant="ghost">
+          <Button onClick={() => setSection("third")} variant="ghost">
             journey
           </Button>
         </div>
         <ThemeSwitch />
       </div>
       <Separator className="my-4" />
-      {state}
+      {sectionState(section)}
     </div>
   );
 }
