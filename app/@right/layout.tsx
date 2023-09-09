51fe65d813b4f27b3f8245a1f81566af04104d7e
Alireza Arezoumandan <comolestyrusistantaneos@gmail.com>
2023-09-09 08:28:51 +0330

    needs work on styles

--- app/@right/layout.tsx
+++ app/@right/layout.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Button } from "../components/ui/button";
 import { Separator } from "../components/ui/seperator";
 import ThemeSwitch from "../components/themeChanger";
+import { motion } from "framer-motion";
 
 export default function RightLayout({
   work,
@@ -12,35 +13,52 @@ export default function RightLayout({
   work: React.ReactNode;
   skills: React.ReactNode;
 }) {
-  const choices = {
-    first: work,
-    second: skills,
-  } as const;
-
   const tabs = [
     { id: "Work", value: work },
     { id: "Skills", value: skills },
   ] as const;
 
   const activeTabState = (state: (typeof tabs)[number]["id"]) => {
-    return choices[state];
+    return tabs.filter((tab) => tab["id"] === state).map((tab) => tab.value);
   };
 
-  const [activeTab, setactiveTab] = useState<keyof typeof choices>("first");
+  const [activeTab, setactiveTab] =
+    useState<(typeof tabs)[number]["id"]>("Work");
 
   return (
     <div className="mt-20 relative">
       <div className="sticky top-0 z-10 bg-background">
         <div className="flex flex-row justify-between items-center pr-2 ">
           <div className="flex flex-row justify-start gap-1 lg:gap-4 p-2 h-14">
-            <Button onClick={() => setactiveTab("first")} variant="ghost">
-              Work
-            </Button>
-            <Separator orientation="vertical" />
-            <Button onClick={() => setactiveTab("second")} variant="ghost">
-              Skills
-            </Button>
-            <Separator orientation="vertical" />
+            {tabs.map((tab) => (
+              <div key={tab.id}>
+                <Button
+                  onClick={() => setactiveTab(tab.id)}
+                  className={`${
+                    activeTab === tab.id ? "" : "hover:text-white/60"
+                  } relative rounded-full px-3 py-1.5 text-sm font-medium text-white outline-sky-400 transition focus-visible:outline-2`}
+                  variant="ghost"
+                  style={{
+                    WebkitTapHighlightColor: "transparent",
+                  }}
+                >
+                  {activeTab === tab.id && (
+                    <motion.span
+                      layoutId="bubble"
+                      className="absolute inset-0 z-10 bg-white mix-blend-difference"
+                      style={{ borderRadius: 9999 }}
+                      transition={{
+                        type: "spring",
+                        bounce: 0.2,
+                        duration: 0.6,
+                      }}
+                    />
+                  )}
+                  {tab.id}
+                </Button>
+                <Separator orientation="vertical" />
+              </div>
+            ))}
           </div>
           <ThemeSwitch />
         </div>
