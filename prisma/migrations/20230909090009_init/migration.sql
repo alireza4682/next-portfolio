e23c04851109f7ac4181e2bdd83988382a5ea155
Alireza <comolestyrusistantaneos@gmail.com>
2023-09-09 12:32:11 +0330

    init migrate

---
+++ prisma/migrations/20230909090009_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "svgs" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "route" TEXT NOT NULL,
+    "url" TEXT NOT NULL
+);
