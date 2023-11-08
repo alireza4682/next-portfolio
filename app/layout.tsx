074a01cccc7c17efcdee601524f9c4ce0d703681
Alireza Arezoumandan <comolestyrusistantaneos@gmail.com>
2023-11-08 20:58:56 +0330

    added bg but not done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { GeistSans, GeistMono } from "geist/font";
 import { Providers } from "./components/providers";
+import styles from "./shadow.module.css";
+import clsx from "clsx";
+import { relative } from "path";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning className="scrollbar-hide">
-      <body className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <body className={inter.className}>
         <Providers>
-          <div className="lg:p-16 lg:bg-foreground">
+          <div className={clsx("relative", "p-20")}>
+            <div className={clsx("absolute", styles.shadow)}></div>
             <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 bg-background rounded-xl p-8 shadow-2xl">
               {left}
               <div className="col-span-1 lg:col-span-2 mx-4">{right}</div>
